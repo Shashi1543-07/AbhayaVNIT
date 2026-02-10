c09cee55d8ec02df0e62386afd5adf020a119db9
SHASHI_DEV <shashitherider2004@gmail.com>
2026-02-10 13:17:05 +0530

    chore: Rename app to Abhaya Connect and update icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'com.vnit.girlsafety',
-    appName: 'vnit-girls-safety',
+    appName: 'Abhaya Connect',
     webDir: 'dist'
 };
 
